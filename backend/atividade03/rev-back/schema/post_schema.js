44192e0ac99f098661f1a107198704b4b2eeec4e
enzo <enzo.danjour@gmail.com>
2021-06-27 14:56:29 -0300

    change schema

--- backend/atividade03/rev-back/schema/post_schema.js
+++ backend/atividade03/rev-back/schema/post_schema.js
@@ -1,8 +1,8 @@
 module.exports = {
     type: "object",
     properties:{
-        titulo: {type: "string", maxLenght: 100, minLenght:5},
-        senha: {type: "string"}
+        titulo: {type: "string", maxLength: 100, minLength:5},
+        texto: {type: "string"}
     },
     required: ["titulo", "texto"],
     additionalProperties: false
