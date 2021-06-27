e3d4e7d433f054d3bc4d11aaf3442813d390930c
enzo <enzo.danjour@gmail.com>
2021-06-27 15:21:06 -0300

    Add ajv

---
+++ backend/atividade03/questao01/schema/product_schema.js
@@ -0,0 +1,10 @@
+module.exports = {
+    type: "object",
+    properties:{
+        nome: {type: "string"},
+        descrição: {type: "string", maxLength: 100, minLength:5},
+        preço: {type: "number"}
+    },
+    required: ["nome", "descrição","preço"],
+    additionalProperties: false
+}
