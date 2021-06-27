db7169734cf4fcbf0876cf483077722028c00c68
enzo <enzo.danjour@gmail.com>
2021-06-27 15:16:29 -0300

    add uuid

--- backend/atividade03/questao01/index.js
+++ backend/atividade03/questao01/index.js
@@ -1,1 +1,17 @@
-const 
+const express = require('express')
+// const product_routes = require('./routes/products_routes')
+
+const app = express()
+app.use(express.json)
+
+// app.use('/produtos',product_routes)
+
+app.get('/',(req,resp)=>{
+    resp.json({
+        msg: 'Hello world'
+    })
+})
+
+app.listen(8080,()=>{
+    console.log('Servidor pronto')
+})
---
+++ backend/atividade03/questao01/routes/products_routes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { v4: uuidv4} = require('uuid')
+const router = express.Router()
+
+products = {}
+
+router.get('/', (req,resp)=>{
+
+})
+
+router.post('/', (req,resp)=>{
+    
+})
+
+router.put('/', (req,resp)=>{
+    
+})
+
+router.delete('/', (req,resp)=>{
+    
+})
+
+
+
+module.exports = router
