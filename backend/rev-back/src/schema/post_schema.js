56fd61d282a5d215ab6c905fd4ebc282dfcfea75
enzo <enzo.danjour@gmail.com>
2021-07-02 20:27:23 -0300

    Add new imputData

--- backend/rev-back/src/schema/post_schema.js
+++ backend/rev-back/src/schema/post_schema.js
@@ -2,8 +2,9 @@ module.exports = {
     type: "object",
     properties:{
         titulo: {type: "string", maxLength: 100, minLength:5},
-        texto: {type: "string"}
+        texto: {type: "string"},
+        userId: {type: "integer"}
     },
-    required: ["titulo", "texto"],
+    required: ["titulo", "texto","userId"],
     additionalProperties: false
 }
