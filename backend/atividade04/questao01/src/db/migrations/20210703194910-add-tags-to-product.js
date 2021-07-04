61d662981cc8d7c7b63a046140c20a922401d6d3
enzo <enzo.danjour@gmail.com>
2021-07-03 23:29:44 -0300

    create migrations

---
+++ backend/atividade04/questao01/src/db/migrations/20210703194910-add-tags-to-product.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tagsProdutos', {
+      id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      produtosId: {
+        type:Sequelize.INTEGER,
+        references:{
+          model: 'Produto',
+          key:'id',
+        },
+        onDelete: 'SET NULL'
+      },
+      produtosId: {
+        type:Sequelize.INTEGER,
+        references:{
+          model: 'Tags',
+          key:'id',
+        },
+        onDelete: 'SET NULL'
+      }
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * 
+     */
+     await queryInterface.dropTable('tagsProdutos');
+  }
+};
--- backend/atividade04/questao01/src/db/models/produto.js
+++ backend/atividade04/questao01/src/db/models/produto.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Produto.hasMany(models.Tags, {foreignKey: 'tagsId'});
+      Produto.belongsToMany(models.Tags, {through: 'TagsProdutos'});
     }
   };
   Produto.init({
